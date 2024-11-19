ec499521481a955ba371c2a795150d29b36d54dc
GRDinculescu <grdinculescu@gmail.com>
2024-11-19 11:54:24 +0100

    Mas errores, todo son errores.

--- Reto4/src/reto4/reto4main.java
+++ Reto4/src/reto4/reto4main.java
@@ -3,12 +3,34 @@ package reto4;
 public class reto4main {
 
 	public static void main(String[] args) {
+		scanner sc = new scanner();
+		/**
+		 * Introduce 10 numeros
+		 * Luego suma los pares con los pares, y los impares con los impares
+		 * Imprima que grupo es mayor
+		 */
 		// TODO Auto-generated method stub
 System.out.println("Empieza el programa de Rafa y Roberto");
 
 
-int h=0,j=0;
+int i=0,j=0;
 string k=0;
+
+for (i = 0, i = 10, i+1){
+	int lkm=funciones.dime("Introduce numero entero ", sc);
+	if (i %= 2 = 0){
+		i += i;
+	}
+	if (j !%= 2 = 0){
+		j += i;
+	}
+}
 int lkm=funciones.dime("Introduce numero entero ", sc);
+
+if (i > j){
+	System.out.println(i +" es mayor a "+"j");
+} else {
+	System.out.println(j+" es ayor a "+i);
+}
 	}
 }
